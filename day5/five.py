c0e879aabbf000a99e8578c78d6a4ea404ddce70
nnnolan <pestanonolan@gmail.com>
2022-12-05 14:20:08 -0600

    wip

--- day5/five.py
+++ day5/five.py
@@ -5,7 +5,7 @@ with open(r'five.txt') as x:
 split_lines = []
 for i in lines:
     split_lines.append(i.split(" "))
-
+letters = []
 one = []
 two = []
 three = []
@@ -21,11 +21,12 @@ for i in split_lines:
         for j in i:
             print(j)
             if count == 1 and "[" in j:
-                one.append(j[count])
+                letters.append(j[count])
                 count =+1
-            elif count == 2 and "[" in j:
-                two.append(i[2])
-                count += 1
+            elif j == "":
+                letters.append("space")
+            
+
     elif i[0] == "1":
         print("initial")
         pass
@@ -33,4 +34,44 @@ for i in split_lines:
         print("move")
         pass
 
-print(one, two)
+
+
+count = 1
+for i in letters:
+    print(i)
+    if count == 1 and i != "space":
+        one.append(i)
+        count += 1
+    
+    elif count == 2 and i != "space":
+        two.append(i)
+        count += 1
+    
+    elif count == 3 and i != "space":
+        three.append(i)
+        count += 1
+    
+    elif count == 4 and i != "space":
+        four.append(i)
+        count += 1
+    
+    elif count == 5 and i != "space":
+        five.append(i)
+        count += 1
+    
+    elif count == 6 and i != "space":
+        six.append(i)
+        count += 1
+
+    elif count == 7 and i != "space":
+        seven.append(i)
+        count += 1
+    
+    elif count == 8 and i != "space":
+        eight.append(i)
+        count = 1
+
+
+
+print(one, two, three, four)
+print(five, six, seven, eight)
